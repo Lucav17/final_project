36197f23d36d9e17ee3ba6e39c86b36188565c01
Zeb Burke-Conte <zmbc@uw.edu>
2016-03-06 13:30:50 -0800

    Remove huge data preload, move to SoQL

--- server.R
+++ server.R
@@ -1,41 +1,29 @@
 # Load packages
 library(leaflet)
 library(jsonlite)
+library(dplyr)
 # Retrieve data
 endpoint_url <- "https://data.seattle.gov/resource/kzjm-xkqj.json"
-num_rows <- as.numeric(fromJSON(paste0(endpoint_url, "?$select=count(type)"))[1,])
-data <- flatten(fromJSON(paste0(endpoint_url, "?$select=*&$order=:id&$limit=50000")))
-# Loop through pages
-# Comment out these lines to test with less data
-for(i in seq(from = 50000, to = num_rows, by = 50000)) {
-  request_url <- paste0(endpoint_url, "?$select=*&$order=:id&$limit=50000&$offset=", i)
-  data <- rbind(data, flatten(fromJSON(request_url)))
-}
-# http://stackoverflow.com/a/22960230/5981634
-data <- data %>% group_by(type, address, latitude, longitude, incident_number) %>% 
-                  filter(row_number() == 1)
-data$id <- seq(1, nrow(data))
-# Convert from unix timestamp
-data <- data %>% mutate(datetime = as.POSIXct(datetime, origin="1970-01-01"))
 
 shinyServer(function(input, output) {
+  live_data <- reactive({
+    fromJSON(paste0(endpoint_url, "?$where=datetime%20IS%20NOT%20NULL%20AND%20latitude%20IS%20NOT%20NULL%20AND%20longitude%20IS%20NOT%20NULL&$order=datetime%20DESC&$limit=15")) %>% 
+      flatten()
+  })
+  
   output$map <- renderLeaflet({
-    live_data <- data %>% ungroup() %>% 
-                  filter(!is.na(latitude), !is.na(longitude)) %>% 
-                  top_n(15, datetime)
-    
     leaflet() %>%
       setView(lat = 47.6097, lng = -122.3331, zoom = 10) %>%
       addProviderTiles("CartoDB.Positron") %>%
       addCircleMarkers(
-        data = live_data,
+        data = live_data(),
         radius = 10,
         fillOpacity = 0.5,
         stroke = FALSE,
         color = 'red',
         lat=~latitude,
         lng=~longitude,
-        layerId=~id
+        layerId=~incident_number
       )
   })
   
@@ -45,10 +33,10 @@ shinyServer(function(input, output) {
     if(is.null(clicked)) {
       return()
     }
-    clicked_data <- data %>% filter(id == clicked$id)
+    clicked_data <- live_data() %>% filter(incident_number == clicked$id)
     
     popup_contents <- as.character(tagList(
-      format(as.POSIXlt(clicked_data$datetime), format = "%D %r"), tags$br(),
+      format(as.POSIXlt(clicked_data$datetime, origin="1970-01-01"), format = "%D %r"), tags$br(),
       clicked_data$address, tags$br(),
       paste0("Type: ", clicked_data$type)
     ))
